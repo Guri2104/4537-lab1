f134b26be1e93fa744d0c51ae42ce42226d119a3
Gurden <gurdensingh@gmail.com>
2021-02-15 13:58:01 -0800

    final changes

--- COMP4537/labs/5/js/write.js
+++ COMP4537/labs/5/js/write.js
@@ -2,10 +2,6 @@ function writeDB(){
     const xhttp = new XMLHttpRequest();
     const name = document.getElementById("name").value;
     let score = document.getElementById("score").value;
-
-    if(Number.isInteger(score) === false){
-        score = 0;
-    }
     
     xhttp.open("GET", "https://karelc.com/COMP4537/labs/5/writeDB?name=" + name + "&score=" + score, true);
     xhttp.send();
